8a62512ba19ca30752b587bed870c17be6f661a3
Paul Sinnett <paul.sinnett@gmail.com>
2021-05-28 08:11:23 +0100

    Initial commit

---
+++ Assets/DebugDraw.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DebugDraw : MonoBehaviour
+{
+	RectTransform rectTransform;
+	RectTransform parentRectTransform;
+	public GUISkin skin;
+
+	void Start()
+	{
+		rectTransform = GetComponent<RectTransform>();
+		parentRectTransform = transform.parent.GetComponent<RectTransform>();
+	}
+
+	void OnGUI()
+	{
+		GUI.skin = skin;
+		Rect rect = rectTransform.rect;
+		rect.center = rectTransform.position;
+		Rect parentRect = parentRectTransform.rect;
+		parentRect.center = parentRectTransform.position;
+		bool inside = rect.Overlaps(parentRect);
+		GUI.Box(rect, $"{rect} {inside}");
+	}
+}
